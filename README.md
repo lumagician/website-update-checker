be3d6f7e7e9702f7928f9909cf710d8353f83bfa
Leo Hofer <33592977+lumagician@users.noreply.github.com>
2022-05-24 15:14:35 +0200

    created venv

--- README.md
+++ README.md
@@ -1,3 +1,18 @@
 # website-update-checker
 
-Regularly checks if the content of the info page for the pre-duty cyber training has changed. Deployed on google cloud as docker container, which is run regularly using cron.
+Regularly checks if the content of the info page for the pre-duty cyber training has changed. Deployed on Linode on a light linux box. The python script is run regularly.
+
+## instructions
+
+setup env
+
+    python3 -m venv venv
+    source venv/bin/activate
+
+install required python packages
+
+    pip install -r requirements.txt
+
+add bot token to environment as variable
+
+    export TELEGRAM_BOT_TOKEN=<YOUR_BOT_TOKEN>
--- app.py
+++ app.py
@@ -2,11 +2,32 @@ from base64 import encode
 import requests
 import hashlib
 from bs4 import BeautifulSoup
+import logging
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CallbackContext, CommandHandler
+
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
+
+async def start(update: Update, context: CallbackContext.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+
+if __name__ == '__main__':
+    application = ApplicationBuilder().token('TOKEN').build()
+    
+    start_handler = CommandHandler('start', start)
+    application.add_handler(start_handler)
+    
+    application.run_polling()
 
 HASH_FILE = "hash.txt"
 
+"""
+
 # get response from website, parse it and get the value of the div in question using its id attribute
-r = requests.get("https://www.vtg.admin.ch/de/aktuell/themen/cyberdefence/cyber-miliz.html#ui-collapse-739")
+r = requests.get(URL)
 soup = BeautifulSoup(r.text, "html.parser")
 content = soup.find(id="collapse_id_content_vtg-internet_de_aktuell_themen_cyberdefence_cyber-miliz_jcr_content_contentPar_accordion_1").get_text()
 
@@ -16,11 +37,10 @@ hash = hashlib.md5(content.encode('utf-8')).hexdigest()
 with open(HASH_FILE, "r") as file:
     hash_old = file.read().rstrip()
 
-    if(hash == hash_old):
-        print("no change")
-    if(hash != hash_old):
-        print("hash changed")
+    # check if empty --> first time running
 
 
 print(hash_old)
 print(hash)
+
+"""
