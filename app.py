b08acfb0289f84e8e84a07131b15655b26e4fb0c
Leo Hofer <33592977+lumagician@users.noreply.github.com>
2022-06-14 23:34:26 +0200

    removed telegram stuff

--- app.py
+++ app.py
@@ -2,45 +2,17 @@ from base64 import encode
 import requests
 import hashlib
 from bs4 import BeautifulSoup
-import logging
-from telegram import Update
-from telegram.ext import ApplicationBuilder, CallbackContext, CommandHandler
 
-logging.basicConfig(
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-    level=logging.INFO
-)
-
-async def start(update: Update, context: CallbackContext.DEFAULT_TYPE):
-    await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
-
-if __name__ == '__main__':
-    application = ApplicationBuilder().token('TOKEN').build()
-    
-    start_handler = CommandHandler('start', start)
-    application.add_handler(start_handler)
-    
-    application.run_polling()
-
-HASH_FILE = "hash.txt"
-
-"""
-
-# get response from website, parse it and get the value of the div in question using its id attribute
-r = requests.get(URL)
+r = requests.get("https://www.vtg.admin.ch/de/aktuell/themen/cyberdefence/cyber-miliz.html#ui-collapse-739")
 soup = BeautifulSoup(r.text, "html.parser")
 content = soup.find(id="collapse_id_content_vtg-internet_de_aktuell_themen_cyberdefence_cyber-miliz_jcr_content_contentPar_accordion_1").get_text()
+hash_updated = hashlib.md5(content.encode('utf-8')).hexdigest()
 
-# hash the value which was extracted above
-hash = hashlib.md5(content.encode('utf-8')).hexdigest()
+f = open("hash.txt", "r")
+hash_from_file = f.read()
 
-with open(HASH_FILE, "r") as file:
-    hash_old = file.read().rstrip()
+if hash_updated == hash_from_file:
+    print("same")
 
-    # check if empty --> first time running
-
-
-print(hash_old)
-print(hash)
-
-"""
+else:
+    print("not same")
