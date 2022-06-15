8d18516c95045e429e8cd2218f6e843fc68d13d0
Leo Hofer <33592977+lumagician@users.noreply.github.com>
2022-06-15 07:07:37 +0200

    program logic implemented
    program logic implemented but file io not quite there yet

--- app/app.py
+++ app/app.py
@@ -6,11 +6,20 @@ hash_new = ""
 # check if hash.txt file already exists
 try:
     with open("./hash.txt", "r") as f:
-        hash_old = f.readline("./hash.txt", "r")
+        hash_old = f.readline()
+        
+        hash_new = getDivContent()
 
+        if hash_old == hash_new:
+            print("no change")
+        else:
+            print("changed")
+
+        f.write(hash_new)
         f.close()
 
+# create if it doesnt already exist
 except FileNotFoundError:
     with open("./hash.txt", "w") as f:
-        f.writeline(getDivContent())
+        f.write(getDivContent())
 
