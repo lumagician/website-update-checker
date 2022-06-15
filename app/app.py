38f360457adac81bf15212876b4ffe7a8a926167
Leo Hofer <33592977+lumagician@users.noreply.github.com>
2022-06-15 14:20:26 +0200

    ifttt webhook notification

--- app/app.py
+++ app/app.py
@@ -1,3 +1,25 @@
+from requests import request
 from updateChecker import checkForUpdates
+import json
+import requests
+import os
 
-print(checkForUpdates())
+url = "https://maker.ifttt.com/trigger/website_changed/json/with/key/"
+
+ifttt_key = os.getenv("IFTTT_KEY")
+ifttt_event_name = os.getenv("IFTTT_EVENT_NAME")
+
+# "dxVc2lttXOQyrvtwUD4_qA"
+
+request_url = url + ifttt_key
+
+payload = json.dumps({
+  "update": checkForUpdates()
+})
+headers = {
+  'Content-Type': 'application/json'
+}
+
+response = requests.request("POST", url, headers=headers, data=payload)
+
+print(response.text)
